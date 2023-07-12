2165b390de06b5b71fc097d440be990c3c6ddd0b
galiardi <galiardi.dev@gmail.com>
2023-07-12 17:34:48 -0400

    Homework fished

--- src/app.js
+++ src/app.js
@@ -1,20 +1,18 @@
 import express, { json } from 'express';
-import routes from './routes/index.js';
 import db from './db.js';
 
 const app = express();
 
 app.use(json());
-app.use('/', routes);
 
 app.get('/', async (req, res) => {
   try {
-    const data = await db.execute('SELECT * FROM user');
-    console.log(data);
-    res.send(data);
+    const [cols, fields] = await db.execute('SELECT NOW()');
+    console.log(cols, fields);
+    res.send(cols);
   } catch (error) {
     console.log(error);
-    res.send({ error: 'Error' });
+    res.send({ error: 'Error al solicitar la información a la base de datos.' });
   }
 });
 
--- src/routes/index.js
+++
@@ -1,7 +0,0 @@
-import { Router } from 'express';
-import userRoutes from './userRoutes.js';
-const router = Router();
-
-router.use('/user', userRoutes);
-
-export default router;
